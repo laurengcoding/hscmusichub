ad57660c029b96f1a495d7b8aef20df241caf6e4
Lauren Guerrera <laurenguerrera@192-168-1-110.tpgi.com.au>
2023-12-10 16:10:20 +1100

    Remove unwanted folders

--- d11/rover/js/main.js
+++
@@ -1,6 +0,0 @@
-// 1) Make the responsive menu work!
-// 2) Make the modal sign up form work!
-//    Show the modal sign up form when someone clicks the "Book your next Rover sitter" link/button.
-//    Want to prevent the browser from jumping to the top of the page when you click on the "Book your..." link? Use `event.preventDefault()`. Read more here:
-//    - https://javascript.info/introduction-browser-events#event-object
-//    - https://javascript.info/default-browser-action
--- d15/exercises/omdb/modal-close.js
+++
@@ -1,6 +0,0 @@
-document.body.addEventListener('click', (event) => {
-  if (!event.target.closest('.modal') || event.target.closest('.modal-content')) {
-    return
-  }
-  modal.classList.remove('show');
-})
